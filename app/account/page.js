fc596028e1928b328bfba404b233a0667b5b8d3c
pejhvaman <pejhvaman@gmail.com>
2025-04-10 15:36:06 +0330

    Initial commit

---
+++ app/account/page.js
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+function Page() {
+  return (
+    <>
+      <Navigation />
+      <h3>your account</h3>
+    </>
+  );
+}
+
+export default Page;
---
+++ app/cabins/page.js
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+function Page() {
+  return (
+    <>
+      <Navigation />
+      <h2>cabins page</h2>
+    </>
+  );
+}
+
+export default Page;
