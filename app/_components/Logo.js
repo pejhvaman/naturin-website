a65913c48e77c1175fdd58167502216de108af32
pejhvaman <pejhvaman@gmail.com>
2025-04-20 12:17:33 +0330

    Done: tailwind config and icons adde

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 function Logo() {
   return (
-    <a href="/" className="flex items-center gap-4 z-10">
+    <Link href="/" className="flex items-center gap-4 z-10">
       <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
       <span className="text-xl font-semibold text-primary-100">Naturin</span>
-    </a>
+    </Link>
   );
 }
 
--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-primary-950">
+      <body className="min-h-screen bg-primary-950 text-primary-50">
         <header>
           <Logo />
           <Navigation />
