daab886d70d45513c31ae81f83b4ceca8842545c
pejhvaman <pejhvaman@gmail.com>
2025-05-26 12:21:52 +0330

    added: reservation section for each cabin and streaming this component

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -12,7 +14,7 @@ function isAlreadyBooked(range, datesArr) {
   );
 }
 
-function DateSelector() {
+function DateSelector({ settings, bookedDates, cabin }) {
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
@@ -20,14 +22,14 @@ function DateSelector() {
   const cabinPrice = 23;
   const range = { from: null, to: null };
 
+  console.log(bookedDates, cabin);
   // SETTINGS
-  const minBookingLength = 1;
-  const maxBookingLength = 23;
+  const { minBookingLength, maxBookingLength } = settings;
 
   return (
     <div className="flex flex-col justify-between">
       <DayPicker
-        className="pt-12 place-self-center"
+        className="[&>*:only-child]:!justify-center"
         mode="range"
         min={minBookingLength + 1}
         max={maxBookingLength}
