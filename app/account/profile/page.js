18b12772789d2c75717bc5c78f9553a345b72a0e
pejhvaman <pejhvaman@gmail.com>
2025-05-30 18:29:18 +0330

    done: updating profile with SA

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,9 +1,16 @@
 import SelectCountry from "@/app/_components/SelectCountry";
 import UpdateProfileForm from "@/app/_components/UpdateProfileForm";
+import { auth } from "@/app/_lib/auth";
+import { getGuest } from "@/app/_lib/data-service";
 
-export default function Page() {
-  //temp
-  const nationality = "Iran";
+export const metadata = {
+  title: "Profile",
+};
+
+export default async function Page() {
+  const session = await auth();
+
+  const guest = await getGuest(session.user.email);
 
   return (
     <div>
@@ -16,12 +23,12 @@ export default function Page() {
         faster and smoother. See you soon!
       </p>
 
-      <UpdateProfileForm>
+      <UpdateProfileForm guest={guest}>
         <SelectCountry
           name="nationality"
           id="nationality"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          defaultCountry={nationality}
+          defaultCountry={guest.nationality}
         />
       </UpdateProfileForm>
     </div>
