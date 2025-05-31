1d44f96a6f357a387da6be75bf9a0c786c45702f
pejhvaman <pejhvaman@gmail.com>
2025-05-31 09:43:36 +0330

    added: manuall cache revalidation and loading indicator for form

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,6 +2,7 @@
 
 import { auth, signIn, signOut } from "@/app/_lib/auth";
 import { supabase } from "./supabase";
+import { revalidatePath } from "next/cache";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -35,4 +36,6 @@ export async function updateGuest(formData) {
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
 }
