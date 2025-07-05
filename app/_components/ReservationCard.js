d094af5055a85e7bab920edf353f462863670814
pejhvaman <pejhvaman@gmail.com>
2025-07-05 17:38:49 +0330

    done: reservation list responsive

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -24,19 +24,19 @@ function ReservationCard({ booking, onDelete }) {
   } = booking;
 
   return (
-    <div className="flex border border-primary-800">
+    <div className="flex flex-col md:flex-row border border-primary-800">
       <div className="relative h-32 aspect-square">
         <Image
           fill
           src={image}
           alt={`Cabin ${name}`}
-          className="object-cover border-r border-primary-800"
+          className="object-cover border-b md:border-r border-primary-800"
         />
       </div>
 
-      <div className="flex-grow px-6 py-3 flex flex-col">
+      <div className="flex-grow px-2 md:px-6 py-3 flex flex-col">
         <div className="flex items-center justify-between">
-          <h3 className="text-xl font-semibold">
+          <h3 className="text-base md:text-xl font-semibold">
             {numNights} nights in Cabin {name}
           </h3>
           {isPast(new Date(startDate)) ? (
@@ -44,13 +44,13 @@ function ReservationCard({ booking, onDelete }) {
               past
             </span>
           ) : (
-            <span className="bg-green-800 text-green-200 h-7 px-3 uppercase text-xs font-bold flex items-center rounded-sm">
+            <span className="bg-green-800 text-green-200 h-7 px-3 uppercase text-[0.5rem] md:text-xs font-bold flex items-center rounded-sm">
               upcoming
             </span>
           )}
         </div>
 
-        <p className="text-lg text-primary-300">
+        <p className="text-xs text-center md:text-left my-2 md:text-lg text-primary-300">
           {format(new Date(startDate), "EEE, MMM dd yyyy")} (
           {isToday(new Date(startDate))
             ? "Today"
@@ -58,23 +58,25 @@ function ReservationCard({ booking, onDelete }) {
           ) &mdash; {format(new Date(endDate), "EEE, MMM dd yyyy")}
         </p>
 
-        <div className="flex gap-5 mt-auto items-baseline">
-          <p className="text-xl font-semibold text-accent-400">${totalPrice}</p>
+        <div className="flex flex-wrap justify-center md:justify-evenly gap-2 md:gap-5 mt-auto items-baseline">
+          <p className="text-sm md:text-xl font-semibold text-accent-400">
+            ${totalPrice}
+          </p>
           <p className="text-primary-300">&bull;</p>
-          <p className="text-lg text-primary-300">
+          <p className="text-sm md:text-lg text-primary-300">
             {numGuests} guest{numGuests > 1 && "s"}
           </p>
-          <p className="ml-auto text-sm text-primary-400">
+          <p className="md:ml-auto text-sm text-primary-400">
             Booked {format(new Date(created_at), "EEE, MMM dd yyyy, p")}
           </p>
         </div>
       </div>
 
       {!isPast(startDate) ? (
-        <div className="flex flex-col border-l border-primary-800 w-[100px]">
+        <div className="flex md:flex-col border-t md:border-l border-primary-800 w-full md:w-[100px]">
           <Link
             href={`/account/reservations/edit/${id}`}
-            className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 border-b border-primary-800 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+            className="group flex justify-center items-center gap-2 uppercase text-xs font-bold text-primary-300 border-r md:border-b border-primary-800 p-4 md:p-0 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
           >
             <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
             <span className="mt-1">Edit</span>
